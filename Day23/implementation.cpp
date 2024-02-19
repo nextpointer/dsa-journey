b22999f1e43055d1cf34b407ebbc7050d6a30d4a
lazy34 <maitysurajit0901@gmail.com>
2024-02-19 21:49:37 +0530

    insert at Head done

---
+++ Day23/implementation.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int data){
+        cout<<"Node is created"<<endl;
+        this->data=data;
+        this->prev=NULL;
+        this->next=NULL;
+    }
+
+};
+
+int getLength(Node* &head){
+    Node* temp=head;
+    int count=0;
+    while (temp!=NULL)
+    {
+        count++;
+        temp=temp->next;
+    }
+    
+    return count;
+    
+}
+
+
+void traverse(Node* &head){
+    Node* temp=head;
+    int size=getLength(head);
+
+    while (temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }  
+
+    cout<<endl;
+}
+
+void insertAtHead(Node* &head,int data)
+{
+    Node* newNode=new Node(data);
+    newNode->next=head;
+    head->prev=newNode;
+    head=newNode;
+    traverse(head);
+}
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    Node* node1=new Node(10);
+    Node* head=node1;
+    Node* tail=node1;
+    traverse(head);
+    insertAtHead(head,6);
+    insertAtHead(head,8);
+    insertAtHead(head,12);
+
+    // insert at Tail
+    // insertAtTail(head,tail,88);
+
+
+
+
+
+
+    return 0;
+}
---
+++ Day23/info.md
@@ -0,0 +1,17 @@
+Date - [03-02-24]
+
+## Topic Name
+
+Linked List
+
+## Todays Goal
+
+* Doubly Linked List
+
+## Code
+
+### Traverse the Linked List
+
+```
+
+```
