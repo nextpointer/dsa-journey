5a2e0fc790d04eacb0846f6788f496881859e4c1
lazy34 <maitysurajit0901@gmail.com>
2024-07-26 09:28:26 +0530

    Tree : Unique Tree possible or not done

---
+++ Tree/27_unique_binary_tree_possibility.cpp
@@ -0,0 +1,25 @@
+// 1 - preorder
+// 2 - inorder
+// 3 - postorder
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool UniqueTreeOrNot(int a, int b)
+    {
+
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution obj;
+    bool possibility = obj.UniqueTreeOrNot(2,3);
+    cout<<possibility<<endl;
+
+    return 0;
+}
