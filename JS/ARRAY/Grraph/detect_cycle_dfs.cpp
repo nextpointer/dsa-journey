9a1883c1a1be6974b02ace91b4f256934860cdb1
lazy34 <maitysurajit0901@gmail.com>
2024-08-23 17:25:03 +0530

    Graph: Detect cycle using DFS

---
+++ JS/ARRAY/Grraph/detect_cycle_dfs.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+    private:
+    bool dfs(int node,int parent,vector<int>adj[],int vis[]){
+        vis[node] = 1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                if(dfs(it,node,adj,vis)==true) return true;
+            }
+            else if(parent!=it){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        int visited[V] ={0};
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(dfs(i,-1,adj,visited)) return true;
+            }
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
