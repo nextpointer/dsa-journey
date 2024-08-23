e7f5f822ce4538554a0b82fa0a234d1d7d3fb3c5
lazy34 <maitysurajit0901@gmail.com>
2024-08-23 16:51:06 +0530

    Graph : Detect cycle With BFS

---
+++ JS/ARRAY/Grraph/detect_cycle_bfs.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+    private:
+    bool detect (int src,vector<int> adjacent[],int vis[]){
+        vis[src] =1;
+        queue<pair<int,int>> q;
+        q.push({src,-1});
+        
+        
+        while(!q.empty()){
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            
+            for(auto adjacentNode: adjacent[node]){
+                if(!vis[adjacentNode]){
+                    vis[adjacentNode] = 1;
+                    q.push({adjacentNode,node});
+                }
+                
+                else if(parent!=adjacentNode){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+        
+    }
+    
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        int visited[V] ={0};
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(detect(i,adj,visited)) return true;
+            }
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
