be547e37e0aaf63f0febb874cd6a8f29a6e240b9
lazy34 <maitysurajit0901@gmail.com>
2024-09-17 19:36:19 +0530

    Smart Pointer is done

--- OOPS/Smart_Pointer.cpp
+++ OOPS/Smart_Pointer.cpp
@@ -1,15 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+// Smart pointer: a C++ wrapper class over a pointer with the extra functionality of automatic object destruction.
+// It overloads the operators like * and ->. The intuition of using a destructor is that it is automatically called when the object goes out of scope.
 
-//smart pointer a c++ warpper class over a pointer with the extra functionality of automatic object desruction.it overloaded the operator like * and->.The intution of use that destructor is automatically called when the object is out of the scope
+class SmartPtr {
+    // Initialize the actual pointer
+    int *ptr;
 
-// now see step by step implementation of Smart Pointer 
-class SmartPtr{
-    
+public:
+    // Initialize the constructor
+    explicit SmartPtr(int *p = NULL) {
+        ptr = p;
+    }
+
+    // Create a destructor that automatically deletes the object
+    ~SmartPtr() {
+        cout << "The pointer deleted automatically" << endl;
+        delete ptr; // Delete the allocated memory
+    }
+
+    // Overloading the dereference operator to use it like a pointer
+    int &operator *() {
+        return *ptr; // Return a reference to the integer pointed to by ptr
+    }
 };
 
-
-int main(){
-
+int main() {
+    SmartPtr ptr(new int()); // Create a SmartPtr that points to a new integer
+    *ptr = 69;               // Assign the value 69 to the integer
+    cout << *ptr << endl;    // Output the value pointed to by ptr
+    return 0;                // ptr goes out of scope here, and the destructor is called
 }
