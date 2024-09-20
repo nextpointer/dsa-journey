15567d27126385ecd810a6c27e2e922448bfc1d1
lazy34 <maitysurajit0901@gmail.com>
2024-09-20 18:48:47 +0530

    oops : copy constructor

--- OOPS/_Implemenations/04_constructor.cpp
+++ OOPS/_Implemenations/04_constructor.cpp
@@ -25,15 +25,38 @@ class Constructor{
 
         // The this points to the current object means contains the current object address
     }
+
+    // Now implement the copy constructor
+    // The copy constructor copy all object properties in the new Object,Nothing complecated
+    Constructor(Constructor &PredefinedObj){
+        this->a = PredefinedObj.a;
+        this->b = PredefinedObj.b;
+        cout<<"The copy constructor is called"<<endl;
+        // Copy constructor is called
+    }
+
+
+    // Create a function for prinint the value
+    void Print(){
+        cout<<"This->"<<this<<endl;
+        cout<<"The value of a "<<this->a<<endl;
+        cout<<"The value of b "<<this->b<<endl;
+    }
 };
 
 
 int main(int argc, char const *argv[])
 {
-    Constructor obj; 
-    // Constructor obj(10,20);
+    // Constructor obj; 
+    Constructor obj(10,20);
+    obj.Print();
 
     // One Constructor is called at a time based on their paramter
+
+    // Create a new Object and copy the value from obj
+    Constructor newObj(obj);
+    newObj.Print();
+
 
     return 0;
 }
